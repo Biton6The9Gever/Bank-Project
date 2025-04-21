f4b7d1a557ad7f0a5a117ca37e8300110c596a39
Biton6The9Gever <yehonatanbiton4@gmail.com>
2025-04-21 23:50:11 +0300

    Ignore .vs folder

---
+++ BankProject/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Data.SQLite;
+
+namespace BankProject
+{
+    internal class Program
+    {
+        public static void CreateTable(string dbFileName)
+        {
+            // Freandly message to the user
+            Console.WriteLine("[==========================================]");
+            Console.WriteLine($"You about to create .db file named \"{dbFileName}\"");
+            Console.WriteLine("are you sure you wanna procced ? [Y/n]");
+            Console.WriteLine("[==========================================]");
+            // Check if the user is sure to create the file
+            if (Console.ReadLine().ToLower() == "y")
+            {
+
+                // Check if the file already exist
+                if (File.Exists(dbFileName))
+                {
+                    Console.WriteLine("ERROR | Table already Exist");
+                }
+                else
+                {
+                    // Try to create the file
+                    try
+                    {
+                        SQLiteConnection.CreateFile(dbFileName);
+                        Console.WriteLine("Database file created!");
+                    }
+                    // If an error occurs, catch it and display a message
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("ERROR | creating database file: " + e.Message);
+                    }
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            CreateTable("Database");
+        }
+    }
+}
---
+++ BankProject/SQLHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace BankProject
+{
+
+    public class SQLHelper
+    {
+        public static string connectionString = "Data Source=Database.db;Version=3;";
+
+        public static int DoQuery(string query)
+        {
+            int rowsAffected = 0;
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("ERROR | While preforming the query" + e.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+            return rowsAffected;
+        }
+    }
+}
